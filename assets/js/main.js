ae8c254a8b5bf5ddfd1787db134b9bcdc9eb129f
Dom123x456 <dominicedgar06@gmail.com>
2022-11-11 12:16:14 +0000

    added const SELECTIONS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,6 @@ const selectionButtons = document.querySelectorAll('[data-selection]')
 const finalColumn = document.querySelector('[data-final-column]')
 const computerScoreSpan = document.querySelector('[data-computer-score]')
 const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
+    
+]
