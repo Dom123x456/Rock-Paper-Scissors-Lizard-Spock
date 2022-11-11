f5f45cc59e2b94896513ad901e43d6ce03eba638
Dom123x456 <dominicedgar06@gmail.com>
2022-11-11 12:19:26 +0000

    Added for each selectionbutton add eventlistner

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,13 @@ const finalColumn = document.querySelector('[data-final-column]')
 const computerScoreSpan = document.querySelector('[data-computer-score]')
 const yourScoreSpan = document.querySelector('[data-your-score]')
 const SELECTIONS = [
-    
+
 ]
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+      const selectionName = selectionButton.dataset.selection
+      const selection = SELECTIONS.find(selection => selection.name === selectionName)
+      makeSelection(selection)
+    })
+  })
